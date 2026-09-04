93b4cedabf2d4118fee4f40653cf0763d9077bc3
Tony <33327990+tonedefdev@users.noreply.github.com>
2026-09-03 22:59:14 -0700

    fix: Ensure we only trim the suffix not replace all dashes (#4)

--- s3.tf
+++ s3.tf
@@ -6,10 +6,10 @@ module "s3" {
   source_file_path    = each.value.spec.source_file_path
   source_file_pattern = each.value.spec.source_file_pattern
 
-  bucket_name = replace(
+  bucket_name = trimsuffix(
     substr(
       "${var.account_id}-${replace(each.key, "_", "-")}-${var.aws_region}", 0, 63
-    ), "-", ""
+    ), "-"
   )
 
   iam_role_arn = each.value.spec.resource_key_ref != null ? format(
